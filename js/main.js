cb138cdbfbb4cb1e1d8efe5f6195aa1bfe6916e2
Renato Lopes de Freitas <renato.tec.lopes@gmail.com>
2022-07-23 23:24:11 -0300

    continuando a aula

---
+++ js/main.js
@@ -0,0 +1,81 @@
+function clique(){
+    alert("Obrigado por clicar");
+}
+
+
+/*Utilizando Funções de calcular (SOMA).
+o símbolo utilizado para calcular é utilizado dentro do return. 
+function soma (n1, n2){
+    return n1 * n2;
+}
+console.log(soma(5, 10));
+
+
+/*Utilizando Data.
+var d = new Date();
+//alert(d);
+//alert(d.getMonth()+1);
+//alert(d.getHours());
+//alert(d.getMinutes());
+alert(d.getFullYear());*/
+
+
+/*Utilizando extrutura de repetição com comando FOR.
+var count;
+for(count = 0; count <= 5; count++){
+console.log(count);
+}*/
+
+/*Utilizando extrutura de repetição WHILE.
+var count = 0;
+while ( count <= 5){
+    console.log(count );
+    alert(count);
+    count++;
+}*/
+
+
+/* Utilizando o atributo (prompt) que faz uma pergunta no exemplo com o comando alert.
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+    }else{
+        alert("menor de idade, Você não pode acessar o site!");
+    }*/
+
+
+/*Exemplo de como usar IF
+var idade = 18;
+if (idade >= 18){
+alert("maior de idade");
+}else{
+    alert("menor de idade");
+}*/
+
+
+/*Exemplo de lista com dicionário adicionado a lista.
+var frutas = [{nome:"Abacate", cor:"Verde"}, {nome:"laranja", cor:"laranja"}];
+console.log(frutas[1]);*/
+
+
+/*Exemplo de dicionário.
+var frutas = {nome:"maça", vermelha:"vermelha"};
+console.log(frutas);*/
+
+/*var frutas = ["uva","Melancia", "Pêra", "Morango"];
+console.log(frutas);
+console.log(frutas.toString())*/
+
+
+//frutas.push("Abacate"); (o atributo push adiciona um íten alista)
+//console.log(frutas); (O atributo POP retina um íten da lista)
+/*console.log(frutas.reverse())
+*var nome = "Renato Lopes";
+var idade1 = 39;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+alert (nome + " tem " + idade + " anos");
+alert(idade1 + idade2);
+console.log(nome);
+console.log(idade1 * idade2);
+console.log(frase.replace("Japão", "Brasil"));*/
